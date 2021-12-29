3a7d05ae4f02c43d98b3e4aa06a3b1e003805b21
JangSubin <sbjang123456@gmail.com>
2021-12-29 14:11:51 +0900

    express server setting

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const routes = require('./routes');
+const { stream } = require('./config/winston')();
+
+const app = express();
+require('./docs/swagger')(app);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extends: false }));
+app.use(
+    morgan(process.env.NODE_ENV !== "production" ? "dev" : "combined", {
+        skip: (req, res) => { return res.statusCode < 400 },
+        stream
+    })
+);
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'content-type, x-access-token');
+    next();
+});
+
+app.use(routes);
+
+app.use((req, res, next) => {
+    res.statusCode = 404;
+    next(Error('not found'));
+});
+
+app.use((err, req, res, next) => {
+    res.status(res.statusCode || 500);
+    res.json({
+        result: false,
+        error: err.message || 'internal server error'
+    });
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,18 @@
+const app = require('./app');
+const http = require('http');
+const path = require('path');
+const yaml = require("js-yaml");
+const fs = require("fs");
+const logger = require('./config/winston')('server');
+const config = yaml.safeLoad(fs.readFileSync(path.join(__dirname, "..", "config", "config.yaml"), 'utf8'))[process.env.NODE_ENV || "development"];
+const PORT = config.comm.nodePort || 8080;
+
+/* Node Server */
+const httpServer = http.createServer(app);
+httpServer.listen(PORT, (err) => {
+    if (err) {
+        logger.error(`Express Server has failed on port: ${PORT}`);
+    }
+    logger.info(`Express server has started on port ${PORT}`);
+});
+
